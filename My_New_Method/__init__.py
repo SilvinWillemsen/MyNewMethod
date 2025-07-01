cd51ec16753696ab49f9ece4e71325f8cd836213
SilvinWillemsen <silvinwillemsen@gmail.com>
2025-07-01 09:14:16 +0200

    Changed simulation.py

---
+++ My_New_Method/__init__.py
@@ -0,0 +1,5 @@
+from .simulation import *
+
+__author__ = "Silvin Willemsen"
+__email__ = "s.willemsen@tue.nl"
+__version__ = "0.1.0" 
---
+++ My_New_Method/simulation.py
@@ -0,0 +1,46 @@
+import json
+import time
+
+
+def check_should_cancel(json_file_path_in):
+    try:
+        if json_file_path_in is not None:
+            with open(json_file_path_in, 'r') as json_file_to_check:
+                data = json.load(json_file_to_check)
+
+        # Update the specified field value
+        if 'should_cancel' in data:
+            return data['should_cancel']
+
+    except Exception as e:
+        print("check_should_cancel returned: " + str(e))
+
+def simulation_method (json_file_path=None):
+
+    result_container = {}
+    with open(json_file_path, 'r') as json_file:
+        result_container = json.load(json_file)
+
+    prev_percent_done = 0
+
+    simulation_length = 150
+    for i in range(simulation_length):
+        percent_done = round(i * 100 / simulation_length)
+        if result_container:
+            if percent_done > prev_percent_done:
+                # Checking whether the user has cancelled the simulation (only one time per percentage increase)
+                if check_should_cancel(json_file_path):
+                    print("breaking out loop")
+                    break
+
+                if result_container:
+                    print(percent_done) # Print to the (Celery) log
+
+                    # Write to the json file to visualise in the front-end
+                    result_container['results'][0]['percentage'] = percent_done
+                    with open(json_file_path, 'w') as percentage_update:
+                        percentage_update.write(
+                            json.dumps(result_container, indent=4)
+                        )
+
+        time.sleep (0.1)
